0d394f9b034492086d08e476fa2e0473c74f0f21
George Marshall <george@georgemarshall.name>
2016-03-04 02:22:08 -0800

    Added more Field types to tests

--- tests/models.py
+++ tests/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
 from django_encrypted.fields import EncryptedField
 
 
 class Test(models.Model):
-    char = models.CharField(_('char'), max_length=50)
-    encrypted_char = EncryptedField(models.CharField(_('char'), max_length=50))
     boolean = models.BooleanField(_('boolean'))
-    encrypted_boolean = EncryptedField(models.BooleanField(_('boolean')))
+    encrypted_boolean = EncryptedField(models.BooleanField())
+    char = models.CharField(_('char'), max_length=50)
+    encrypted_char = EncryptedField(models.CharField(max_length=50))
+    decimal = models.DecimalField(_('decimal'))
+    encrypted_decimal = EncryptedField(models.DecimalField())
+    duration = models.DurationField(_('duration'))
+    encrypted_duration = EncryptedField(models.DurationField())
+    date = models.DateField(_('date'), auto_now=True)
+    encrypted_date = EncryptedField(models.DateField(auto_now=True))
+    float = models.FloatField(_('float'))
+    encrypted_float = EncryptedField(models.FloatField())
     integer = models.IntegerField(_('integer'))
-    encrypted_integer = EncryptedField(models.IntegerField(_('integer')))
-    datetime = models.DateField(_('datetime'), auto_now=True)
-    encrypted_datetime = EncryptedField(models.DateField(_('datetime'), auto_now=True))
+    encrypted_integer = EncryptedField(models.IntegerField())
+    ip_addres = models.GenericIPAddressField(_('ip address'))
+    encrypted_ip_addres = EncryptedField(models.GenericIPAddressField())
+    text = models.TextField(_('text'))
+    encrypted_text = EncryptedField(models.TextField())
+    uuid = models.UUIDField(_('uuid'))
+    encrypted_uuid = EncryptedField(models.UUIDField())
--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,3 +1,7 @@
+import uuid
+from datetime import timedelta
+from decimal import Decimal
+
 from django.conf import settings
 from django.test import TestCase
 from django.utils.six import binary_type
@@ -12,16 +16,31 @@ class EncryptedFieldTestCase(TestCase):
         self.assertIsInstance(key, binary_type)
 
     def test_field_match(self):
-        obj = Test(char='Hello, world!', boolean=True, integer=42)
-        obj.encrypted_char = obj.char
+        obj = Test(boolean=True, char='Hello, world!',
+                   decimal=Decimal('1.00'), duration=timedelta(1),
+                   float=42.0, integer=42, ip_addres='::1',
+                   text='Too short...', uuid=uuid.uuid4())
         obj.encrypted_boolean = obj.boolean
+        obj.encrypted_char = obj.char
+        obj.encrypted_decimal = obj.decimal
+        obj.encrypted_duration = obj.duration
+        obj.encrypted_float = obj.float
         obj.encrypted_integer = obj.integer
+        obj.encrypted_ip_addres = obj.ip_addres
+        obj.encrypted_text = obj.text
+        obj.encrypted_uuid = obj.uuid
         obj.save()
         del obj
 
         # Fetch the object
         obj = Test.objects.get()
-        self.assertEqual(obj.char, obj.encrypted_char)
         self.assertEqual(obj.boolean, obj.encrypted_boolean)
+        self.assertEqual(obj.char, obj.encrypted_char)
+        self.assertEqual(obj.date, obj.encrypted_date)
+        self.assertEqual(obj.decimal, obj.encrypted_decimal)
+        self.assertEqual(obj.duration, obj.encrypted_duration)
+        self.assertEqual(obj.float, obj.encrypted_float)
         self.assertEqual(obj.integer, obj.encrypted_integer)
-        self.assertEqual(obj.datetime, obj.encrypted_datetime)
+        self.assertEqual(obj.ip_addres, obj.encrypted_ip_addres)
+        self.assertEqual(obj.text, obj.encrypted_text)
+        self.assertEqual(obj.uuid, obj.encrypted_uuid)
