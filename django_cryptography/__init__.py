6392bcba7d2c1435f20dfb9f8a155a4229b8b3a9
George Marshall <george@georgemarshall.name>
2016-05-21 08:48:17 -0700

    First stable release

--- django_cryptography/__init__.py
+++ django_cryptography/__init__.py
@@ -1,5 +1,5 @@
 from django_cryptography.utils.version import get_version
 
-VERSION = (0, 1, 0, 'rc', 1)
+VERSION = (0, 1, 0, 'final', 0)
 
 __version__ = get_version(VERSION)
--- setup.py
+++ setup.py
@@ -148,7 +148,7 @@ setup(
     author_email='george@georgemarshall.name',
     license='BSD',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
