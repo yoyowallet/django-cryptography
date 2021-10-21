8307c82a84821b8a3cf35dc717da59013167e44b
George Marshall <george@georgemarshall.name>
2021-10-20 23:56:23 -0700

    Migrate to GitHub Actions

---
+++ runtests.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
+
+    django.setup()
+
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["tests"])
+    dbfile = settings.DATABASES.get('default', {}).get('NAME')
+    if dbfile and os.path.exists(dbfile):
+        os.remove(dbfile)
+    sys.exit(bool(failures))
+
+
+if __name__ == "__main__":
+    main()
